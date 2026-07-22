d831f433ffecb5753ef59d2ea425e2b43c5fc5ba
maxwong3 <42724546+maxwong3@users.noreply.github.com>
2026-07-22 00:52:18 -0400

    Established Postgres connection module in db.ts

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,10 +1,36 @@
 import express, { type Express, type Request, type Response } from 'express';
+import pool from './config/db';
 
 const app: Express = express();
+const VERSION = '/api/v1';
+const PORT = 5000
 
 app.get('/', (req: Request, res: Response) => {
     res.send('Hello world!');
-})
+});
 
-app.listen(5000);
+app.get(`${VERSION}/health`, (req: Request, res: Response) => {
+    res.status(200).json({
+        status: "OK",
+        message: `Backend server is running on port ${PORT}.`
+    });
+});
 
+app.get(`${VERSION}/testdb`, async (req: Request, res: Response) => {
+    try {
+        const result = await pool.query("SELECT NOW();");
+
+        res.status(200).json({
+            status: "Database connected",
+            time: result.rows[0].now
+        });
+    } catch (error) {
+        console.error(error);
+
+        res.status(500).json({
+            status: "Database connection failed."
+        });
+    }
+});
+
+app.listen(PORT);
