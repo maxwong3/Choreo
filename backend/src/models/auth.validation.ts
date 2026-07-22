f6de69f42533f3b56a5359d408cd358321dcd428
maxwong3 <42724546+maxwong3@users.noreply.github.com>
2026-07-22 14:52:05 -0400

    auth schemas

---
+++ backend/src/models/auth.validation.ts
@@ -0,0 +1,14 @@
+import { z } from "zod";
+
+export const registerSchema = z.object({
+    email: z.email("Please provide a valid email address."),
+    username: z.string().min(3, "Username too short."),
+    password: z.string().min(8, "Password must be at least 8 characters long."),
+    firstName: z.string().min(1, "First name is required."),
+    lastName: z.string().min(1, "Last name is required."),
+});
+
+export const loginSchema = z.object({
+    username: z.string().min(3, "Username too short."),
+    password: z.string().min(1, "Password is required."),
+});
