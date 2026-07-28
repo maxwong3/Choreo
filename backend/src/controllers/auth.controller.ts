e57a22d9026d6243e70bb4ee9d9b80a37a1e9bfa
maxwong3 <42724546+maxwong3@users.noreply.github.com>
2026-07-28 14:27:58 -0400

    Bug fixes

--- backend/src/controllers/auth.controller.ts
+++ backend/src/controllers/auth.controller.ts
@@ -50,7 +50,7 @@ export class AuthController {
 
             const result = await AuthService.login(req.body);
 
-            res.status(200).json({message: "Login successful.", result});
+            res.status(200).json(result);
         } catch (err) {
             next(err);
         }
